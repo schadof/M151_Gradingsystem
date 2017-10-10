e871b6952e79fec3fe50fe63666e77cae4eaaa87
Michael van der Heide <vanderheide.michael@gmail.com>
2017-10-10 22:10:36 +0200

    Added login, logout and user creation functions

---
+++ assets/php/login.php
@@ -0,0 +1,55 @@
+<?php
+
+//Conects to Database and returns connection
+function connectDB(){
+    $connection = mysqli_connect("localhost", "root", "", "greades"); // Establishing connection with server..
+    return $connection;
+}
+
+//Returns list of students
+function getStudents (){
+    $query = mysqli_query(connectDB(), "select id, name from users");
+    $students = mysqli_fetch_object($query);
+    mysqli_close(connectDB()); // Closing Connection
+    return $students;
+}
+
+//Sets Session Tokens if password and username in post were correct
+function login() {
+    // Define $username and $password
+    $name = $_POST['username'];
+    $password = md5($_POST['password']);
+    // Establishing Connection with Server by passing server_name, user_id, password and database as a parameter
+    $name = stripslashes($name);
+    $password = stripslashes($password);
+    $password = mysqli_real_escape_string(connectDB(), $password);
+    // SQL query to fetch information of registerd users and finds user match.
+    $query = mysqli_query(connectDB(), "select * from users where `Name` like '".$name."' AND Password like '".$password."'");
+    $rows = mysqli_num_rows($query);
+    if ($rows == 1) {
+        $_SESSION['login_user'] = $name; // Initializing Session
+    } else {
+        $error = "Username or Password is invalid";
+        echo $error;
+        $_SESSION['login_failure'] = 'true';
+    }
+    mysqli_close(connectDB()); // Closing Connection
+}
+
+//Logs out current user
+function logout() {
+    @session_destroy();
+}
+
+//Creates user
+function createUser() {
+    $name=$_POST['username'];
+    $password=md5($_POST['password']);
+    $name = stripslashes($name);
+    $password = stripslashes($password);
+    $password = mysqli_real_escape_string(connectDB(), $password);
+    $query = mysqli_query(connectDB(), "insert into `users` (`Name`, Password) VALUES ('".$name."', '".$password."')");
+    mysqli_close(connectDB()); // Closing Connection
+}
+
+?>
