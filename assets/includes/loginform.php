cc03ef7aabaeb1af4ebb55b28d69a3ff19a4b593
schadof <lars.ragutt@gmx.ch>
2017-10-13 21:38:41 +0100

    Beautify

---
+++ assets/includes/loginform.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<div id="form">
+    <h1 id="title">Enter Login Data</h1>
+    <div id="login">
+        <form action="/login.php">
+            Username:<br>
+            <input type="text" name="username" placeholder="Username">
+            <br>
+            Password:<br>
+            <input type="password" name="password" placeholder="Password">
+            <br><br>
+            <input type="submit" value="Submit">
+        </form>
+        <a href="HTML/register.php">register</a>
+    </div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Greades</title>
+    <meta name="description" content="The Great Gradingsystem">
+    <meta name="author" content="Lars Ragutt">
+
+    <link rel="stylesheet" href="assets/css/layout.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <?php
+    include 'default.php';
+    ?>
+
+</head>
+
+<body>
+<?php
+include 'assets/includes/loginform.php';
+?>
+</body>
+</html>
