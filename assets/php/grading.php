c04739c0638aa29f683294e67cdd78c3330a2fe5
schadof <lars.ragutt@gmx.ch>
2017-10-26 16:32:25 +0200

    Added SQL Select;

--- assets/php/grading.php
+++ assets/php/grading.php
@@ -1,54 +1,34 @@
 <?php
-if ($result->num_rows > 0) {
-    echo "<table>";
-    echo "<tr><th>Note</th><th>Wertung</th><th>Beschreibung</th></tr>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["question"] . " name='q" . $x . "'</td>";
-        echo "<td>" . $row["answer"] . " name='a" . $x . "'</td>";
-        echo "<td>" . $row["worth"] . " name='w" . $x . "'</td>";
-        echo "<td><label>mark</label></td>";
-        $x++;
-    }
-    echo "<tr><td class='add'><input type='text' class='textbox' name='q" . $x . "'></td>";
-    echo "<td class='add'><input type='text' class='textbox' name='a" . $x . "'></td>";
-    echo "<td class='add'><input type='number' name='w" . $x . "'></td>";
-    echo "</table>";
+
+$con = connectDB();
+
+//check connection
+if (!$con){
+    exit("Connection Failed: " . $con);
 }
 
+/* show tables */
+$sql = "
+SELECT u.fname, u.lname, c.name, ma.mark, ma.weight, ma.description, mo.module FROM users u
+JOIN classes c
+ON u.class = c.id
+JOIN marks ma
+ON ma.user = u.id
+JOIN modules mo
+ON ma.module = mo.id;";
+$result = odbc_exec($con,$sql);
+//check SQL
+if (!$result){
+    exit("Error in SQL");
 }
-echo "<script>"
-    . "function attachCheckboxHandlers() {"
-    //loading all elements with class name point
-    . "var tops = document.getElementsByClassName('point');"
-    //loop runs while there are elements by from class point
-    . "for (var i=0, len=tops.length; i<len; i++) {"
-    . "if ( tops[i].type === 'checkbox' ) {"
-    //when checkbox with nr. i is checked run updateTotal
-    . "tops[i].onclick = updateTotal;"
-    . "}"
-    . "}"
-    . "}"
-    . "function updateTotal(e) {"
-    . "var form = this.form;"
-    //setting content from val to label f_result
-    . "var val = parseFloat( document.getElementById('f_result').textContent );"
-    //if checked
-    . "if ( this.checked ) {"
-    //calculating points
-    . "val += parseFloat(this.value);"
-    //printing points
-    . "document.getElementById('f_result').innerHTML = val;"
-    //calculating mark
-    . "document.getElementById('mark').innerHTML = Math.round((val*5/" . $total . "+1) * 100) / 100;"
-    . "}"
-    //if unchecked
-    . "else {"
-    . "val -= parseFloat(this.value);"
-    . "document.getElementById('f_result').innerHTML = val;"
-    . "document.getElementById('mark').innerHTML = Math.round((val*5/" . $total . "+1) * 100) / 100;"
-    . "}"
-    . "}"
-    . "</script>";
+
+echo "<table>";
+while (odbc_fetch_row($result))
+{
+    $note=odbc_result($result,"Note");
+
+    echo "<tr><td>$note</td>";
 }
-echo "<script>attachCheckboxHandlers();</script>";
+echo "</table>";
+odbc_close($con);
 ?>
