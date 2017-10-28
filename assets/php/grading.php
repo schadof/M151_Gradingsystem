2536662153214ee0700e47a42f552cdea85a9913
schadof <lars.ragutt@gmx.ch>
2017-10-28 18:04:04 +0200

    Marks of differnt modules are now in different tables.

--- assets/php/grading.php
+++ assets/php/grading.php
@@ -7,29 +7,45 @@ if (!$con){
     exit("Connection Failed: " . $con);
 }
 
-/* show tables */
 $sql = "
+SELECT module FROM modules;";
+
+$result = odbc_exec($con,$sql);
+
+while(odbc_fetch_row($result))
+{
+	$module = odbc_result($result,"module");
+/* show tables */
+$sql1 = "
 SELECT u.fname, u.lname, c.name, ma.mark, ma.weight, ma.description, mo.module FROM users u
 JOIN classes c
 ON u.class = c.id
 JOIN marks ma
 ON ma.user = u.id
 JOIN modules mo
-ON ma.module = mo.id;";
-$result = odbc_exec($con,$sql);
+ON ma.module = mo.id
+WHERE mo.module = '$module';";
+$result1 = odbc_exec($con,$sql1);
 //check SQL
-if (!$result){
+if (!$result1){
     exit("Error in SQL");
 }
 
-echo "<table>";
-while (odbc_fetch_row($result))
+if(odbc_num_rows($result1) != 0)
 {
-    $mark=odbc_result($result,"mark");
-    $weight=odbc_result($result,"weight");
+echo $module;
+}
+
+echo "<table>";
+while (odbc_fetch_row($result1))
+{
+    $mark=odbc_result($result1,"mark");
+    $weight=odbc_result($result1,"weight");
 
     echo "<td>$mark" . "x" . "$weight</td>";
 }
 echo "</table>";
+echo "</br>";
+}
 odbc_close($con);
 
